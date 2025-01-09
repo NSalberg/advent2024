00b1e5fa6f2183c716850cc154d2f714f5b569c4
Nathan Salberg <natesalberg@gmail.com>
2025-01-09 14:21:48 -0600

    condensed day 5

--- solutions/day05/day05.go
+++ solutions/day05/day05.go
@@ -22,38 +22,27 @@ func createOrderingMap(lines string) map[int][]int {
 	return rules
 }
 
-func createCmpFunc(orderMap map[int][]int) func(a, b int) int {
-	return func(a, b int) int {
-		for _, ruleNumber := range orderMap[a] {
-			if b == ruleNumber {
+func solve(input string, sorted bool) int {
+	split := strings.Split(input, "\n\n")
+
+	rules := createOrderingMap(split[0])
+
+	cmp := func(a, b string) int {
+		c, _ := strconv.Atoi(a)
+		d, _ := strconv.Atoi(b)
+		for _, ruleNumber := range rules[c] {
+			if d == ruleNumber {
 				return -1
 			}
 		}
 		return 0
 	}
-}
-
-func solve(input string, sort bool) int {
-	lines := strings.Split(input, "\n\n")
 
 	total := 0
-
-	rules := createOrderingMap(lines[0])
-	cmp := createCmpFunc(rules)
-	for _, line := range strings.Fields(lines[1]) {
-		sPageNumbers := strings.Split(line, ",")
-		pageNumbers := make([]int, len(sPageNumbers))
-
-		for i, snum := range sPageNumbers {
-			pageNumbers[i], _ = strconv.Atoi(snum)
-		}
-
-		if isSorted := slices.IsSortedFunc(pageNumbers, cmp); isSorted && !sort {
-			middleNum := pageNumbers[len(pageNumbers)/2]
-			total += middleNum
-		} else if !isSorted && sort {
-			slices.SortFunc(pageNumbers, cmp)
-			middleNum := pageNumbers[len(pageNumbers)/2]
+	for _, line := range strings.Fields(split[1]) {
+		if sPageNumbers := strings.Split(line, ","); slices.IsSortedFunc(sPageNumbers, cmp) == sorted {
+			slices.SortFunc(sPageNumbers, cmp)
+			middleNum, _ := strconv.Atoi(sPageNumbers[len(sPageNumbers)/2])
 			total += middleNum
 		}
 
